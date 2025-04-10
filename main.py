6b1e875390fcd4064a28a7dbee0dc946d9615139
serhii <serhii.lundiak@gmail.com>
2025-04-10 13:06:56 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI,Path,Query,HTTPException,status
+import uvicorn
+from bs4 import BeautifulSoup
+import re 
+from aiohttp import ClientSession
+import asyncio
+from requests_html import HTMLSession,AsyncHTMLSession
+import requests
+
+app = FastAPI()
+
+@app.get("/find/{tag}")
+def find_text(
+    tag: str = Path(..., description="p"),
+    url: str = Query(..., description="https://uk.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0"),
+    text: str = Query(..., description="Украї́на (МФА: [ʊkrɐˈjinɐ]ⓘ) — держава у Східній та")
+):
+    response = requests.get(url)
+    if response.status_code == 200:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    elements = soup.find_all(tag)
+
+    for element in elements:
+        if re.search(text, element.get_text()):
+            return {"found": element.get_text(strip=True)}
+
+    raise HTTPException(status_code=404, detail="Текст не знайдено")
